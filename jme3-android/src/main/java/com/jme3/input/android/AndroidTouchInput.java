cbf5493ba7b2f8812bb9dbd0f0b0596fdabceeb4
rickard <rickard@mindemia.com>
2020-08-13 14:36:56 +0200

    testing crlf

--- jme3-android/src/main/java/com/jme3/input/android/AndroidTouchInput.java
+++ jme3-android/src/main/java/com/jme3/input/android/AndroidTouchInput.java
@@ -74,7 +74,7 @@ public class AndroidTouchInput implements TouchInput {
     protected int numPointers = 0;
     final private HashMap<Integer, Vector2f> lastPositions = new HashMap<Integer, Vector2f>();
     final private ConcurrentLinkedQueue<InputEvent> inputEventQueue = new ConcurrentLinkedQueue<InputEvent>();
-    private final static int MAX_TOUCH_EVENTS = 1024;
+    private final static int MAX_TOUCH_EVENTS = 1024; 
     private final TouchEventPool touchEventPool = new TouchEventPool(MAX_TOUCH_EVENTS);
     private float scaleX = 1f;
     private float scaleY = 1f;
